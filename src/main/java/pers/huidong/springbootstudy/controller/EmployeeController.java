f084cf6ffff7bd3863a7893b880e428e99ed5976
Test <2804234243@qq.com>
2020-05-25 00:55:24 +0800

    Commit 6-1 : crud

--- src/main/java/pers/huidong/springbootstudy/controller/EmployeeController.java
+++ src/main/java/pers/huidong/springbootstudy/controller/EmployeeController.java
@@ -1,10 +1,13 @@
 package pers.huidong.springbootstudy.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import pers.huidong.springbootstudy.dao.DepartmentDao;
 import pers.huidong.springbootstudy.dao.EmployeeDao;
+import pers.huidong.springbootstudy.entities.Department;
 import pers.huidong.springbootstudy.entities.Employee;
 
 import java.util.Collection;
@@ -20,11 +23,49 @@ public class EmployeeController {
 
     @Autowired
     EmployeeDao employeeDao;
+    @Autowired
+    DepartmentDao departmentDao;
 
-    @GetMapping("emps")
+    @GetMapping("/emps")
     public String list(Model model){
         Collection<Employee> employees = employeeDao.getAll();
         model.addAttribute("emps",employees);
         return "emp/list";
+    }
+
+    @GetMapping("/emp")
+    public String toAddPage(Model model){
+        //来到添加页面前需要查出所有的部门，在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id,Model model){
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp",employee);
+
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts",departments);
+        return "emp/add";
+    }
+    @PutMapping("/emp")
+    public String  updateEmployee(Employee employee){
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        System.out.println(id);
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
