786f9d0f52e8612e184c3e07fbd72932fe8df0b9
Test <2804234243@qq.com>
2020-05-25 21:45:59 +0800

    Commit 7-3 : 定制servlet

---
+++ src/main/java/pers/huidong/springbootstudy/config/MyServletConfig.java
@@ -0,0 +1,20 @@
+package pers.huidong.springbootstudy.config;
+
+import org.springframework.boot.autoconfigure.web.embedded.EmbeddedWebServerFactoryCustomizerAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @program: springboot-study
+ * @auther: HuiDong
+ * @date: 2020/5/25 17:32
+ * @description:
+ */
+//@Configuration
+//public class MyServletConfig {
+//
+//    public EmbeddedWebServerFactoryCustomizerAutoConfiguration embeddedWebServerFactoryCustomizerAutoConfiguration(){
+//        return new EmbeddedWebServerFactoryCustomizerAutoConfiguration(){
+//            public void customize()
+//        }
+//    }
+//}
