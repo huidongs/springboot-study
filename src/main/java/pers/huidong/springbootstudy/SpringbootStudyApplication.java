82fa53dd08a7a57101cd31e634192e453b72f4dd
Test <2804234243@qq.com>
2020-05-26 21:42:30 +0800

    Commit 8-3 : datasource

--- src/main/java/pers/huidong/springbootstudy/SpringbootStudyApplication.java
+++ src/main/java/pers/huidong/springbootstudy/SpringbootStudyApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootStudyApplication {
 
+    //启动入口
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStudyApplication.class, args);
     }
